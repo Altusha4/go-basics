fc18282ee439a68985b4f31cfdcda07cd8204768
Altusha4 <240782@astanait.edu.kz>
2025-12-15 15:38:23 +0600

    Add exercise 4: switch statement for weekdays and weekend

--- ex-4-switch/main.go
+++ ex-4-switch/main.go
@@ -1,1 +1,16 @@
-package ex_4_switch
+package main
+
+import "fmt"
+
+func main() {
+	day := "Saturday"
+
+	switch day {
+	case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday":
+		fmt.Println("Workday")
+	case "Saturday", "Sunday":
+		fmt.Println("Weekend")
+	default:
+		fmt.Println("Unknown day")
+	}
+}
